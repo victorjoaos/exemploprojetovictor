32db67bc20cc1448fed4bbbe258ced488fd3de36
Victor Soares <119049313+victorjoaos@users.noreply.github.com>
2022-11-25 11:39:47 +0000

    comentario

--- main.cs
+++ main.cs
@@ -1,7 +1,10 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        //comentario
+    }
 }
